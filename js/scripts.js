a94ab18dcd7a47790277749dbc5f67bd307ed047
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-15 10:42:15 -0700

    program gathers user input for first, last, and telephone

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,14 @@ function AddressBook() {
 addressBook = new AddressBook();
 
 //user interface logic
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const firstName = $("input[name=firstName]").val();
+    const lastName = $("input[name=lastName]").val();
+    const telephoneNumber = $("input[name=telephoneNumber]").val();
+    alert(firstName);
+    alert(lastName);
+    alert(telephoneNumber);
+  })
+})
